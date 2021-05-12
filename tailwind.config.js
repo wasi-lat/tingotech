39effd994bda723de008e2d97bd54ea3fbc3b5b7
paulotijero <paulotijero@gmail.com>
2021-05-12 00:31:15 -0500

    STYLE: add custom style by testimonials slides

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'second': '#113423',
+      }
+    },
   },
   variants: {
     extend: {},
