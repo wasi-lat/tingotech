9a5abc47b4bdb035da4ac7a927079ce82b5b220b
paulotijero <paulotijero@gmail.com>
2021-05-17 23:47:51 -0500

    STYLE: fix styles navbar

--- components/layout.js
+++ components/layout.js
@@ -55,7 +55,7 @@ export default function Layout({ children }) {
         <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet" />
       </Head>
 
-      <header>
+      <header className="absolute w-full py-3 px-2">
         <Navbar />
       </header>
 
