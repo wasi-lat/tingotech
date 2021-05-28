db9f6600d56aaa9f03a7f418c3c9830ce685a675
paulotijero <paulotijero@gmail.com>
2021-05-27 23:23:28 -0500

    ADD: original text in testimonial slides

--- components/homepage/testimonials-slides.js
+++ components/homepage/testimonials-slides.js
@@ -1,25 +1,26 @@
-import Carousel from 'react-multi-carousel';
+import Carousel from "react-multi-carousel";
+import { User } from 'react-feather'
 
 const responsive = {
   superLargeDesktop: {
     breakpoint: { max: 4000, min: 3000 },
-    items: 1
+    items: 1,
   },
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 1
+    items: 1,
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 1
+    items: 1,
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
-    items: 1
-  }
+    items: 1,
+  },
 };
 
-export default function TestimonialsSlides(){
+export default function TestimonialsSlides() {
   return (
     <Carousel
       responsive={responsive}
@@ -31,29 +32,64 @@ export default function TestimonialsSlides(){
       dotListClass="dot"
     >
       <div className="relative container mx-auto pt-16 pb-10 flex flex-col items-center">
-        <img className="py-5" src="/img/double-quotes.png" alt="Double Quotes" />
+        <img
+          className="py-5"
+          src="/img/double-quotes.png"
+          alt="Double Quotes"
+        />
         <p className="my-8 pb-5 font-light italic text-center text-white w-full md:w-5/12">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu est eu diam 
-          condimentum quis. Tristique adipiscing mauris orci, velit purus consequat lectus. 
-          Lectus arcu quis habitasse urna pulvinar nec nunc erat id.
+          Buen servicio, realiza delivery tanto la recepción como el envío, la
+          entrega es a tiempo y precios competitivos.
         </p>
         <div className="block w-14 h-14 overflow-hidden rounded-xl bg-second">
-          <img className="object-contain w-full h-full" src="/img/brands/empresa_1.png" alt="" />
+          <User className="object-contain w-full h-full text-white opacity-75" />
+          {/* <img
+            className="object-contain w-full h-full"
+            src="/img/brands/empresa_1.png"
+            alt=""
+          /> */}
         </div>
-        <p className="pt-2 pb-5 font-medium text-white italic opacity-100">Kevin Juancho</p>
+        <p className="pt-2 pb-5 font-medium text-white italic opacity-100">
+          Johann Espinoza Jorge
+        </p>
       </div>
       <div className="relative container mx-auto pt-16 pb-10 flex flex-col items-center">
-        <img className="py-5" src="/img/double-quotes.png" alt="Double Quotes" />
+        <img
+          className="py-5"
+          src="/img/double-quotes.png"
+          alt="Double Quotes"
+        />
         <p className="my-8 pb-5 font-light italic text-center text-white w-full md:w-5/12">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu est eu diam 
-          condimentum quis. Tristique adipiscing mauris orci, velit purus consequat lectus. 
-          Lectus arcu quis habitasse urna pulvinar nec nunc erat id.
+          Excelente servicio 😊
         </p>
         <div className="block w-14 h-14 overflow-hidden rounded-xl bg-second">
-          <img className="object-contain w-full h-full" src="/img/brands/empresa_1.png" alt="" />
+          <User className="object-contain w-full h-full text-white opacity-75" />
+          {/* <img
+            className="object-contain w-full h-full"
+            src="/img/brands/empresa_1.png"
+            alt=""
+          /> */}
         </div>
-        <p className="pt-2 pb-5 font-medium text-white italic opacity-100">El Juancho</p>
+        <p className="pt-2 pb-5 font-medium text-white italic opacity-100">
+          Juliet Laguna Cierto
+        </p>
+      </div>
+      <div className="relative container mx-auto pt-16 pb-10 flex flex-col items-center">
+        <img
+          className="py-5"
+          src="/img/double-quotes.png"
+          alt="Double Quotes"
+        />
+        <p className="my-8 pb-5 font-light italic text-center text-white w-full md:w-5/12">
+          5 estrellas sin comentario
+        </p>
+        <div className="block w-14 h-14 overflow-hidden rounded-xl bg-second">
+          <User className="object-contain w-full h-full text-white opacity-75" />
+        </div>
+        <p className="pt-2 pb-5 font-medium text-white italic opacity-100">
+          Christian Peralta
+        </p>
       </div>
     </Carousel>
-  )
+  );
 }
