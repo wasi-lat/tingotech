255dc6ab206e3b092a93508bba3b6e0ef2fa6cdc
paulotijero <paulotijero@gmail.com>
2021-05-27 23:42:06 -0500

    CHORE: run prettier format

--- components/shared/navbar.js
+++ components/shared/navbar.js
@@ -7,7 +7,11 @@ export default function Navbar() {
         <li>
           <Link href="/">
             <a title="Tingo Texh" className="">
-              <img className="object-contain w-full h-full" src="/img/logo.png" alt="Tingo Tech" />
+              <img
+                className="object-contain w-full h-full"
+                src="/img/logo.png"
+                alt="Tingo Tech"
+              />
             </a>
           </Link>
         </li>
@@ -15,22 +19,35 @@ export default function Navbar() {
       <ul className="flex items-center overflow-auto text-sm">
         <li className="hidden md:block text-center w-24 md:m-0">
           <Link scroll={true} href="#about-us">
-            <a title="Nosotros" className="text-white font-medium">Nosotros</a>
+            <a title="Nosotros" className="text-white font-medium">
+              Nosotros
+            </a>
           </Link>
         </li>
         <li className="hidden md:block text-center w-24 md:m-0">
           <Link scroll={true} href="#services">
-            <a title="Servicios" className="text-white font-medium">Servicios</a>
+            <a title="Servicios" className="text-white font-medium">
+              Servicios
+            </a>
           </Link>
         </li>
         <li className="hidden md:block text-center w-24 md:m-0">
           <Link scroll={true} href="#team">
-            <a title="Equipo" className="text-white font-medium">Equipo</a>
+            <a title="Equipo" className="text-white font-medium">
+              Equipo
+            </a>
           </Link>
         </li>
         <li className="text-center w-24 bg-primary py-1 rounded md:m-0">
           <Link href="https://api.whatsapp.com/send?phone=51986077683&text=Hola Tío TingoTech te queria consultar...">
-            <a target="_blank" rel="noopener noreferrer" title="Contáctanos" className="text-white font-medium cursor-pointer">Contáctanos</a>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              title="Contáctanos"
+              className="text-white font-medium cursor-pointer"
+            >
+              Contáctanos
+            </a>
           </Link>
         </li>
       </ul>
