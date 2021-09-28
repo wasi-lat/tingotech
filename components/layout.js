c3d9b2bf38c6a19e78530e466d0eecf9f2cbb80d
JimmyLoloy98 <loloy.laurencio@gmail.com>
2021-09-28 17:41:03 -0500

    UPDATE: update meta description and hero description

--- components/layout.js
+++ components/layout.js
@@ -10,7 +10,7 @@ export default function Layout({ children }) {
       <Head> {/* <!-- Primary Meta Tags --> */}
         <title>TingoTech - Inicio | Servicio Técnico de Laptops y PC's</title>
         <link rel="icon" href="/assets/img/logo.png" />
-        <meta name="description" content="TingoTech soluciones tecnologicas generando productividad." />
+        <meta name="description" content="Bridamos servicio técnico de Laptops y PCs. Mantenimiento preventivo y correctivo. Instalación de programas en general. Asesoría técnica personalizada. Transparencia y puntualidad nos diferencia." />
         <meta name="language" content="es" />
         <meta name="author" content="TingoTech" />
         <meta name="subject" content="TingoTech soluciones tecnologicas generando productividad." />
