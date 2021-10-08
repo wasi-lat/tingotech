42ef51b86b0962381f008f6b68fcb72140528732
JimmyLoloy98 <loloy.laurencio@gmail.com>
2021-10-08 00:28:49 -0500

    UPDATE: social media icons in navbar

--- components/shared/navbar.js
+++ components/shared/navbar.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { Phone, Facebook, Instagram, } from 'react-feather'
 
 export default function Navbar() {
   return (
@@ -6,7 +7,7 @@ export default function Navbar() {
       <ul className="mr-3 text-2xl md:text-3xl">
         <li className="h-12 md:h-20">
           <Link href="/">
-            <a title="Tingo Texh">
+            <a title="Tingo Tech">
               <img
                 className="object-contain h-full"
                 src="/img/tingotech.png"
@@ -16,7 +17,7 @@ export default function Navbar() {
           </Link>
         </li>
       </ul>
-      <ul className="flex items-center overflow-auto text-sm">
+      <ul className="flex items-center justify-items-end overflow-auto text-sm">
         <li className="hidden md:block text-center w-24 md:m-0">
           <Link scroll={true} href="#about-us">
             <a title="Nosotros" className="text-white font-medium">
@@ -38,17 +39,58 @@ export default function Navbar() {
             </a>
           </Link>
         </li>
-        <li className="text-center w-24 bg-primary py-1 rounded md:m-0  hover:opacity-90">
-          <Link href="https://api.whatsapp.com/send?phone=51928408503&text=Hola Tío TingoTech te quería consultar...">
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              title="Contáctanos"
-              className="text-white font-medium"
-            >
-              Contáctanos
-            </a>
-          </Link>
+        <li className="text-center w-60 md:m-0">
+          <ul className="flex content-evenly items-center overflow-auto text-sm">
+            <li className="text-center w-1/4 md:m-0">
+              <Link href="https://www.facebook.com/tingotech">
+                <a
+                  className="flex items-center justify-center"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  title="Facebook"
+                >
+                  <Facebook size={18} className="text-white font-normal stroke-2"/>
+                </a>
+              </Link>
+            </li>
+            <li className="text-center w-1/4 md:m-0">
+              <Link scroll={true} href="https://www.instagram.com/tingotech">
+                <a
+                  className="flex content-center justify-center"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  title="Instagram"
+                >
+                  <Instagram size={18} className="center text-white font-normal stroke-2"/>
+                </a>
+              </Link>
+            </li>
+            <li className="text-center w-1/4 md:m-0">
+              <Link scroll={true} href="https://api.whatsapp.com/send?phone=51928408503&text=Hola Tío TingoTech te quería consultar...">
+                <a
+                  className="flex content-center justify-center"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  title="Whatsapp"
+                >
+                  <svg className="h-5 w-5">
+                    <use xlinkHref="#whatsapp" />
+                  </svg>
+                </a>
+              </Link>
+            </li>
+            <li className="text-center w-1/4 md:m-0 justify-center">
+              <Link href="tel:+51-928-408-503">
+                <a
+                  className="flex items-center justify-center"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                <Phone size={18} className="text-white font-normal stroke-2"/>
+                </a>
+              </Link>
+            </li>
+          </ul>
         </li>
       </ul>
     </nav>
